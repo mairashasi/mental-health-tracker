87478e5fd671fcf518d9599054fc9ab08c0ecba5
maira.shasmeen <maira.shasmeen@gmail.com>
2024-08-28 14:14:29 +0700

    update username

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h%sz5hn631&t3s2*!)v3k3k2i@+7l=anv=b)+q$82t&0tisy91
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "maira-shasmeen-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 
