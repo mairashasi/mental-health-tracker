fa98ffab52a87b52f51983c9ea38977d4f60c690
maira.shasmeen <maira.shasmeen@gmail.com>
2024-09-11 14:28:37 +0700

    test drive tutorial 2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,5 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://maira-shasmeen-mentalhealthtracker.pbp.cs.ui.ac.id", "https://maira-shasmeen-mentalhealthtracker.pbp.cs.ui.ac.id"]
